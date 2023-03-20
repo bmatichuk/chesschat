6ba3fe1608c65a15900aa933b46c20f0d59ef6e3
Bruce Matichuk <bmatichuk@gmail.com>
2023-03-19 22:51:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import chess
 import chess.svg
+from IPython.display import SVG, display
 
-st.set_page_config(page_title="Streamlit Chess Board")
+@st.cache(allow_output_mutation=True)
+def get_board():
+    return chess.Board()
 
-board = chess.Board()
+board = get_board()
 
 st.title("Streamlit Chess Board")
 
@@ -18,7 +21,44 @@ if move:
     else:
         st.success("Moved successfully!")
 
+piece_map = {
+    'P': '♙',
+    'N': '♘',
+    'B': '♗',
+    'R': '♖',
+    'Q': '♕',
+    'K': '♔',
+    'p': '♟',
+    'n': '♞',
+    'b': '♝',
+    'r': '♜',
+    'q': '♛',
+    'k': '♚'
+}
+
+def get_piece(symbol):
+    return piece_map.get(symbol, '')
+
+def get_square(x, y):
+    file = chess.FILE_NAMES[x]
+    rank = chess.RANK_NAMES[7 - y]
+    return file + rank
+
 svg = chess.svg.board(board=board)
+
+pieces = list(board.fen().split()[0])
+
+for i, piece in enumerate(pieces):
+    x = i % 8
+    y = i // 8
+    square = get_square(x, y)
+    symbol = get_piece(piece)
+    if symbol:
+        svg = svg.replace(f'id="{square}"', f'id="{square}" onclick="handle_click(\'{square}\')"')
+
+def handle_click(square):
+    st.write(f"Selected square: {square}")
+    
 html = f"<div style='width: 400px; height: 400px;'>{svg}</div>"
 st.markdown(html, unsafe_allow_html=True)
 
